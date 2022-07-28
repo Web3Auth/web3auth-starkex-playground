0f4cd36326bd04b48ae9311451b3396c709a2a76
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-07-28 18:54:19 +0800

    Update Console.tsx

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -3,8 +3,8 @@ const Console = () => {
     <div className="flex-col w-full">
       <h1 className="flex justify-center md:items-center items-center font-medium leading-tight text-3xl mt-0 mb-2 text-slate-600">Response</h1>
       <div className="justify-center w-full">
-        <div className="md:flex md:items-center p-4 m-4 bg-gray-200" id="console">
-          <p></p>
+        <div className="md:flex md:items-center p-4 m-4 bg-gray-200 overflow-scroll" id="console">
+          <p className="font-mono overflow-scroll	"></p>
         </div>
       </div>
     </div>
