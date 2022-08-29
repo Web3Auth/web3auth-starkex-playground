2135e9d791d6dc8a9fab1d5454c99af88d26a07b
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-08-29 08:45:26 +0530

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,14 +2,15 @@ import { Dispatch, SetStateAction } from "react";
 
 interface FormProps {
   heading?: string;
+  headingCenter?: boolean;
   formDetails: { label: string; input: string; readOnly?: boolean; onChange?: any }[];
   children?: JSX.Element | JSX.Element[];
 }
 
-function Form({ heading, formDetails, children }: FormProps) {
+function Form({ heading, headingCenter, formDetails, children }: FormProps) {
   return (
     <div className="w-11/12 px-4 sm:px-6 lg:px-8  z-0">
-      <p className="text-lg font-bold">{heading}</p>
+      <p className={`text-lg font-bold ${headingCenter ? "text-center" : ""}`}>{heading}</p>
       <form action="" className="p-8 mt-6 mb-0 space-y-4 rounded-lg bg-white">
         {formDetails.map((formDetail) => (
           <div key={formDetail.label}>
