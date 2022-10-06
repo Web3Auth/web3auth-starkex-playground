ec908f8712d6455de90c971f87a06827408e6915
Saviour1001 <harshghodkar@gmail.com>
2022-10-06 20:52:30 +0530

    got the data

--- src/services/starkexProvider.ts
+++ src/services/starkexProvider.ts
@@ -9,7 +9,10 @@ import { Contract, providers, Wallet } from "ethers";
 import ABI from "../config/ABI.json";
 import { IWalletProvider } from "./walletProvider";
 
-const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (...args: unknown[]) => void): IWalletProvider => {
+const starkexProvider = (
+  provider: SafeEventEmitterProvider | null,
+  uiConsole: (...args: unknown[]) => void
+): IWalletProvider => {
   const starkExAPI = new StarkExAPI({
     endpoint: "https://gw.playground-v2.starkex.co",
   });
@@ -17,7 +20,9 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
   const getETHAddress = async (): Promise<any> => {
     try {
       const privateKey = await provider?.request({ method: "private_key" });
-      const address = privateToAddress(Buffer.from(privateKey as string, "hex")).toString("hex");
+      const address = privateToAddress(
+        Buffer.from(privateKey as string, "hex")
+      ).toString("hex");
       return address;
     } catch (error) {
       uiConsole(error);
@@ -29,7 +34,10 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
     try {
       const privateKey = await provider?.request({ method: "private_key" });
       const keyPair = starkwareCrypto.ec.keyFromPrivate(privateKey, "hex");
-      const account = starkwareCrypto.ec.keyFromPublic(keyPair.getPublic(true, "hex"), "hex");
+      const account = starkwareCrypto.ec.keyFromPublic(
+        keyPair.getPublic(true, "hex"),
+        "hex"
+      );
       uiConsole(account);
       return account;
     } catch (error) {
@@ -72,7 +80,11 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
     }
   };
 
-  const onMintRequest = async (amount: string, tokenId: string, vaultId: string) => {
+  const onMintRequest = async (
+    amount: string,
+    tokenId: string,
+    vaultId: string
+  ) => {
     try {
       const txId = await starkExAPI.gateway.getFirstUnusedTxId();
       const starkKey = await getStarkKey();
@@ -93,26 +105,55 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
 
   const onViewBalanceRequest = async (assetType: string, vaultId: string) => {
     const privateKey = await provider?.request({ method: "private_key" });
-    const alchemyProvider = new providers.AlchemyProvider("goerli", "4ZLZLLlFuTk2Md56571LEpcn6WML4L7X");
+    const alchemyProvider = new providers.AlchemyProvider(
+      "goerli",
+      "4ZLZLLlFuTk2Md56571LEpcn6WML4L7X"
+    );
     const signer = new Wallet(privateKey as string, alchemyProvider);
-    const StarkExchange = new Contract("0x471bDA7f420de34282AB8AF1F5F3DAf2a4C09746", ABI, signer);
+    const StarkExchange = new Contract(
+      "0x471bDA7f420de34282AB8AF1F5F3DAf2a4C09746",
+      ABI,
+      signer
+    );
     const starkKey = await getStarkKey();
-    uiConsole(await StarkExchange.getDepositBalance(BigInt(`0x${starkKey}` as string).toString(10), assetType, vaultId));
+    uiConsole(
+      await StarkExchange.getDepositBalance(
+        BigInt(`0x${starkKey}` as string).toString(10),
+        assetType,
+        vaultId
+      )
+    );
   };
 
-  const onL1DepositRequest = async (amount: string, assetType: string, vaultId: string) => {
+  const onL1DepositRequest = async (
+    amount: string,
+    assetType: string,
+    vaultId: string
+  ) => {
     uiConsole("Depositing from L1");
     try {
       const privateKey = await provider?.request({ method: "private_key" });
-      const alchemyProvider = new providers.AlchemyProvider("goerli", "4ZLZLLlFuTk2Md56571LEpcn6WML4L7X");
+      const alchemyProvider = new providers.AlchemyProvider(
+        "goerli",
+        "4ZLZLLlFuTk2Md56571LEpcn6WML4L7X"
+      );
       const signer = new Wallet(privateKey as string, alchemyProvider);
-      const StarkExchange = new Contract("0x471bDA7f420de34282AB8AF1F5F3DAf2a4C09746", ABI, signer);
+      const StarkExchange = new Contract(
+        "0x471bDA7f420de34282AB8AF1F5F3DAf2a4C09746",
+        ABI,
+        signer
+      );
       const starkKey = await getStarkKey();
-      const txn = await StarkExchange.depositEth(BigInt(`0x${starkKey}` as string).toString(10), assetType, vaultId, {
-        gasPrice: 10000000000,
-        gasLimit: 9000000,
-        value: amount,
-      });
+      const txn = await StarkExchange.depositEth(
+        BigInt(`0x${starkKey}` as string).toString(10),
+        assetType,
+        vaultId,
+        {
+          gasPrice: 10000000000,
+          gasLimit: 9000000,
+          value: amount,
+        }
+      );
       uiConsole(txn);
       uiConsole(await txn.wait());
     } catch (error) {
@@ -121,7 +162,11 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
     }
   };
 
-  const onDepositRequest = async (amount: string, tokenId: string, vaultId: string) => {
+  const onDepositRequest = async (
+    amount: string,
+    tokenId: string,
+    vaultId: string
+  ) => {
     try {
       const starkKey = await getStarkKey();
 
@@ -143,14 +188,28 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
     }
   };
 
-  const onViewDepositBalanceRequest = async (assetId: string, vaultId: string) => {
+  const onViewDepositBalanceRequest = async (
+    assetId: string,
+    vaultId: string
+  ) => {
     try {
       const starkKey = await getStarkKey();
       const privateKey = await provider?.request({ method: "private_key" });
-      const alchemyProvider = new providers.AlchemyProvider("goerli", "4ZLZLLlFuTk2Md56571LEpcn6WML4L7X");
+      const alchemyProvider = new providers.AlchemyProvider(
+        "goerli",
+        "4ZLZLLlFuTk2Md56571LEpcn6WML4L7X"
+      );
       const signer = new Wallet(privateKey as string, alchemyProvider);
-      const StarkExchange = new Contract("0x471bDA7f420de34282AB8AF1F5F3DAf2a4C09746", ABI, signer);
-      const balance = await StarkExchange.getQuantizedDepositBalance(BigInt(`0x${starkKey}` as string).toString(10), assetId, vaultId);
+      const StarkExchange = new Contract(
+        "0x471bDA7f420de34282AB8AF1F5F3DAf2a4C09746",
+        ABI,
+        signer
+      );
+      const balance = await StarkExchange.getQuantizedDepositBalance(
+        BigInt(`0x${starkKey}` as string).toString(10),
+        assetId,
+        vaultId
+      );
       uiConsole(balance);
     } catch (error) {
       console.log(error);
@@ -158,17 +217,33 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
     }
   };
 
-  const onL1WithdrawalRequest = async (amount: string, vaultId: string, assetType: string) => {
+  const onL1WithdrawalRequest = async (
+    amount: string,
+    vaultId: string,
+    assetType: string
+  ) => {
     uiConsole("Withdrawing to L1");
     try {
       const privateKey = await provider?.request({ method: "private_key" });
-      const alchemyProvider = new providers.AlchemyProvider("goerli", "4ZLZLLlFuTk2Md56571LEpcn6WML4L7X");
+      const alchemyProvider = new providers.AlchemyProvider(
+        "goerli",
+        "4ZLZLLlFuTk2Md56571LEpcn6WML4L7X"
+      );
       const signer = new Wallet(privateKey as string, alchemyProvider);
-      const StarkExchange = new Contract("0x471bDA7f420de34282AB8AF1F5F3DAf2a4C09746", ABI, signer);
-      const txn = await StarkExchange.withdrawFromVault(assetType, vaultId, amount, {
-        gasPrice: 10000000000,
-        gasLimit: 9000000,
-      });
+      const StarkExchange = new Contract(
+        "0x471bDA7f420de34282AB8AF1F5F3DAf2a4C09746",
+        ABI,
+        signer
+      );
+      const txn = await StarkExchange.withdrawFromVault(
+        assetType,
+        vaultId,
+        amount,
+        {
+          gasPrice: 10000000000,
+          gasLimit: 9000000,
+        }
+      );
       uiConsole(txn);
       uiConsole(await txn.wait());
     } catch (error) {
@@ -177,7 +252,11 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
     }
   };
 
-  const onWithdrawalRequest = async (amount: string, tokenId: string, vaultId: string) => {
+  const onWithdrawalRequest = async (
+    amount: string,
+    tokenId: string,
+    vaultId: string
+  ) => {
     try {
       const txId = await starkExAPI.gateway.getFirstUnusedTxId();
       const starkKey = await getStarkKey();
@@ -255,70 +334,85 @@ const starkexProvider = (provider: SafeEventEmitterProvider | null, uiConsole: (
     }
   };
 
-  const onSettlementRequest = async (settlementInfo: any, party_a_order: any, party_b_order: any): Promise<any> => {
+  const onSettlementRequest = async (
+    settlementInfo: any,
+    party_a_order: any,
+    party_b_order: any
+  ): Promise<any> => {
     try {
       const account = await getStarkAccount();
       const publicKeyX = account.pub.getX().toString("hex");
       const publicKeyY = account.pub.getY().toString("hex");
       const tx_id = await starkExAPI.gateway.getFirstUnusedTxId();
-      const response = await fetch("https://gw.playground-v2.starkex.co/v2/gateway/add_transaction", {
-        headers: {
-          accept: "application/json, text/plain, */*",
-        },
-        body: JSON.stringify({
-          tx: {
-            party_b_order: {
-              expiration_timestamp: party_b_order.expirationTimestamp,
-              eth_address: `${party_b_order.ethAddress}`,
-              amount_buy: `${party_b_order.amountBuy}`,
-              amount_sell: `${party_b_order.amountSell}`,
-              fee_info: {
-                fee_limit: `${party_b_order.feeInfo.feeLimit}`,
-                token_id: `${party_b_order.feeInfo.tokenId}`,
-                source_vault_id: `${party_b_order.feeInfo.sourceVaultId}`,
-              },
-              order_type: party_b_order.orderType,
-              vault_id_sell: party_b_order.vaultIdSell,
-              nonce: party_b_order.nonce,
-              token_sell: `${party_b_order.tokenSell}`,
-              token_buy: `${party_b_order.tokenBuy}`,
-              vault_id_buy: party_b_order.vaultIdBuy,
-              type: party_b_order.type,
-            },
-            settlement_info: {
-              party_a_sold: "75",
-              party_b_sold: "25",
-              party_b_fee_info: { fee_taken: "15", destination_vault_id: 80, destination_stark_key: "0x60" },
-              party_a_fee_info: { fee_taken: "15", destination_vault_id: 80, destination_stark_key: "0x60" },
-            },
-            party_a_order: {
-              expiration_timestamp: party_a_order.expirationTimestamp,
-              amount_buy: `${party_a_order.amountBuy}`,
-              signature: {
-                s: `${party_a_order.signature.s}`,
-                r: `${party_a_order.signature.r}`,
-              },
-              amount_sell: `${party_a_order.amountSell}`,
-              fee_info: {
-                fee_limit: `${party_a_order.feeInfo.feeLimit}`,
-                token_id: `${party_a_order.feeInfo.tokenId}`,
-                source_vault_id: party_a_order.feeInfo.sourceVaultId,
-              },
-              order_type: party_a_order.orderType,
-              vault_id_sell: party_a_order.vaultIdSell,
-              nonce: party_a_order.nonce,
-              public_key: party_a_order.publicKey,
-              token_sell: `${party_a_order.tokenSell}`,
-              token_buy: `${party_a_order.tokenBuy}`,
-              vault_id_buy: party_a_order.vaultIdBuy,
-              type: `${party_a_order.type}`,
-            },
-            type: "SettlementRequest",
+      const response = await fetch(
+        "https://gw.playground-v2.starkex.co/v2/gateway/add_transaction",
+        {
+          headers: {
+            accept: "application/json, text/plain, */*",
           },
-          tx_id: `${tx_id}`,
-        }),
-        method: "POST",
-      });
+          body: JSON.stringify({
+            tx: {
+              party_b_order: {
+                expiration_timestamp: party_b_order.expirationTimestamp,
+                eth_address: `${party_b_order.ethAddress}`,
+                amount_buy: `${party_b_order.amountBuy}`,
+                amount_sell: `${party_b_order.amountSell}`,
+                fee_info: {
+                  fee_limit: `${party_b_order.feeInfo.feeLimit}`,
+                  token_id: `${party_b_order.feeInfo.tokenId}`,
+                  source_vault_id: `${party_b_order.feeInfo.sourceVaultId}`,
+                },
+                order_type: party_b_order.orderType,
+                vault_id_sell: party_b_order.vaultIdSell,
+                nonce: party_b_order.nonce,
+                token_sell: `${party_b_order.tokenSell}`,
+                token_buy: `${party_b_order.tokenBuy}`,
+                vault_id_buy: party_b_order.vaultIdBuy,
+                type: party_b_order.type,
+              },
+              settlement_info: {
+                party_a_sold: "75",
+                party_b_sold: "25",
+                party_b_fee_info: {
+                  fee_taken: "15",
+                  destination_vault_id: 80,
+                  destination_stark_key: "0x60",
+                },
+                party_a_fee_info: {
+                  fee_taken: "15",
+                  destination_vault_id: 80,
+                  destination_stark_key: "0x60",
+                },
+              },
+              party_a_order: {
+                expiration_timestamp: party_a_order.expirationTimestamp,
+                amount_buy: `${party_a_order.amountBuy}`,
+                signature: {
+                  s: `${party_a_order.signature.s}`,
+                  r: `${party_a_order.signature.r}`,
+                },
+                amount_sell: `${party_a_order.amountSell}`,
+                fee_info: {
+                  fee_limit: `${party_a_order.feeInfo.feeLimit}`,
+                  token_id: `${party_a_order.feeInfo.tokenId}`,
+                  source_vault_id: party_a_order.feeInfo.sourceVaultId,
+                },
+                order_type: party_a_order.orderType,
+                vault_id_sell: party_a_order.vaultIdSell,
+                nonce: party_a_order.nonce,
+                public_key: party_a_order.publicKey,
+                token_sell: `${party_a_order.tokenSell}`,
+                token_buy: `${party_a_order.tokenBuy}`,
+                vault_id_buy: party_a_order.vaultIdBuy,
+                type: `${party_a_order.type}`,
+              },
+              type: "SettlementRequest",
+            },
+            tx_id: `${tx_id}`,
+          }),
+          method: "POST",
+        }
+      );
       return response;
     } catch (error) {
       return error as string;
